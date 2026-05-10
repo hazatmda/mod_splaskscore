10fafcb56aa7f2687a0ff386d2b5c3e6ebe295ad
Hazim <azizan.hazim@mda.gov.my>
2026-05-10 16:04:47 +0800

    Implement Phase 1 Joomla module layout foundation

---
+++ tmpl/default.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  mod_splaskscore
+ *
+ * @copyright   Copyright (C) 2025 Muhammad Azizan Hazim
+ * @license     GNU General Public License version 3 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+require __DIR__ . '/modern_circle.php';

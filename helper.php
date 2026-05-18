6a33ce8f1047c9867adb9c17b0084fce815d4de6
Hazim <azizan.hazim@mda.gov.my>
2026-05-18 10:19:05 +0800

    Release v1.6.5 metadata synchronization

--- helper.php
+++ helper.php
@@ -29,7 +29,7 @@ final class ModSplaskscoreHelper
         12 => 'Disember',
     ];
 
-    private const ENGINE_VERSION = '1.6.4';
+    private const ENGINE_VERSION = '1.6.5';
 
     private const DEFAULT_DUPLICATE_COOLDOWN_MINUTES = 10;
 

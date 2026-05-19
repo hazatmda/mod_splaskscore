b9342ea5ffd7d219d670aaa1d347d9bddde632ad
Hazim <azizan.hazim@mda.gov.my>
2026-05-19 11:10:34 +0800

    Fix release artifact packaging and bump version to 1.6.8

--- helper.php
+++ helper.php
@@ -29,7 +29,7 @@ final class ModSplaskscoreHelper
         12 => 'Disember',
     ];
 
-    private const ENGINE_VERSION = '1.6.7';
+    private const ENGINE_VERSION = '1.6.8';
 
     private const DEFAULT_DUPLICATE_COOLDOWN_MINUTES = 10;
 

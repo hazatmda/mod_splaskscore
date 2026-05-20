b7701a36df8f02a50e2f8eef286a2c02debf338a
Hazim <azizan.hazim@mda.gov.my>
2026-05-20 13:16:14 +0800

    Include fields directory in release package builds

---
+++ fields/AboutMetadata.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Case-preserving alias for environments/package checks that expect
+ * fields/AboutMetadata.php while the canonical field implementation
+ * remains in fields/aboutmetadata.php.
+ */
+
+defined('_JEXEC') or die;
+
+require_once __DIR__ . '/aboutmetadata.php';
